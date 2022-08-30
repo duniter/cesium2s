6ae0062cda3513f4b2503eff00da1f214c411d10
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-08-30 19:05:26 +0200

    initial import

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'app.cesium',
   appName: 'Cesium2',
   webDir: 'www',
   bundledWebRuntime: false
