91b872908c4de784cab38a7d6d703b53788df196
Benoit Lavenier <benoit.lavenier@e-is.pro>
2022-09-01 19:11:58 +0200

    [enh] Home: add login method selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {PlatformService} from "./shared/services/platform.service";
 import {environment} from "@environments/environment";
+import {AccountService} from "@app/wallet/account.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -21,9 +23,17 @@ export class AppComponent {
     { title: 'MENU.WOT', url: '/wot', icon: 'people' },
 
     { title: 'MENU.SETTINGS', url: '/settings', icon: 'settings' },
+
+    { title: 'HOME.BTN_CHANGE_ACCOUNT', icon: 'log-out', color: 'danger',
+
+      handle: (event) => this.logout(event),
+      enable: () => this.accountService.isLogin
+    },
   ];
 
-  constructor(private platform: PlatformService) {
+  constructor(private platform: PlatformService,
+              private accountService: AccountService,
+              private router: Router) {
     this.start();
   }
 
@@ -34,5 +44,12 @@ export class AppComponent {
     await this.platform.start();
 
     console.info(`[app] Starting [OK] in ${Date.now()-now}ms`);
+  }
+
+  async logout(event) {
+    this.accountService.forgetAll();
+    await this.router.navigateByUrl('/home', {
+      replaceUrl: true
+    });
   }
 }
