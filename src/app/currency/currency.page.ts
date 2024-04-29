33b5830f51010c8b65ab193348e92ef5fc4bc8af
Carles Barrobés <carles@barrobes.com>
2024-04-29 16:30:57 +0200

    Add data to currency page
    Start using api constants in currency page

--- src/app/currency/currency.page.ts
+++ src/app/currency/currency.page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { Currency } from '@app/currency/currency.model';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { NetworkService } from '@app/network/network.service';
 import { AppPage, AppPageState } from '@app/shared/pages/base-page.class';
+import { TranslateService } from '@ngx-translate/core';
+import { u32, u64 } from '@polkadot/types';
 import { RxState } from '@rx-angular/state';
 
+export interface CurrencyParameters {
+  currencyName: SafeHtml;
+  currencyNetwork: string;
+  members: number;
+  monetaryMass: number;
+  unitsPerUd: number;
+  udCreationPeriodMs: number;
+  udReevalPeriodMs: number;
+}
+
 export interface CurrencyPageState extends AppPageState {
-  currency: Currency;
+  params: { [key: string]: CurrencyParameters };
 }
 
 @Component({
@@ -15,16 +27,59 @@ export interface CurrencyPageState extends AppPageState {
   providers: [RxState],
 })
 export class CurrencyPage extends AppPage<CurrencyPageState> {
-  currency: Currency;
+  params: { [key: string]: CurrencyParameters };
 
-  constructor(protected networkService: NetworkService) {
+  @Input() showAllRules: boolean;
+  @Input()
+  get showUnitsInDU(): boolean {
+    return this.units === 'du';
+  }
+  set showUnitsInDU(showInDu: boolean) {
+    this.units = showInDu ? 'du' : 'base';
+  }
+  units: 'du' | 'base' = 'base';
+
+  constructor(
+    protected networkService: NetworkService,
+    protected translate: TranslateService,
+    private sanitizer: DomSanitizer
+  ) {
     super({ name: 'currency-service' });
   }
 
   protected async ngOnLoad(): Promise<Partial<CurrencyPageState>> {
+    this.showAllRules = false;
+    this.showUnitsInDU = false;
     const networkService = await this.networkService.ready();
-    this.currency = networkService.currency;
-    console.log(this.currency);
-    return { currency: this.currency };
+    const api = networkService.api;
+    const currency = networkService.currency;
+    const fractionsPerUnit = Math.pow(10, currency.decimals);
+    const [monetaryMassFractions, members] = await Promise.all([
+      api.query.universalDividend.monetaryMass(),
+      api.query.membership.counterForMembership(),
+    ]);
+    const duValue = (api.consts.universalDividend.unitsPerUd as u64).toNumber() / fractionsPerUnit;
+    this.params = {
+      base: {
+        currencyName: currency.displayName,
+        currencyNetwork: currency.network,
+        monetaryMass: (monetaryMassFractions as u64).toNumber() / fractionsPerUnit,
+        members: (members as u32).toNumber(),
+        unitsPerUd: duValue,
+        udCreationPeriodMs: (api.consts.universalDividend.udCreationPeriod as u64).toNumber(),
+        udReevalPeriodMs: (api.consts.universalDividend.udReevalPeriod as u64).toNumber(),
+      },
+      du: {
+        currencyName: this.sanitizer.bypassSecurityTrustHtml(`${this.translate.instant('UD')}<sub>${currency.displayName}</sub>`),
+        currencyNetwork: currency.network,
+        monetaryMass: (monetaryMassFractions as u64).toNumber() / fractionsPerUnit / duValue,
+        members: (members as u32).toNumber(),
+        unitsPerUd: 1,
+        udCreationPeriodMs: (api.consts.universalDividend.udCreationPeriod as u64).toNumber(),
+        udReevalPeriodMs: (api.consts.universalDividend.udReevalPeriod as u64).toNumber(),
+      },
+    };
+
+    return { params: this.params };
   }
 }
