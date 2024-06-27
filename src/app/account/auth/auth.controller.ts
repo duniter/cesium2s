bce665a2e5d19be0183b9a9b60cc267dd3ac919d
Hugo Trentesaux <hugo@trentesaux.fr>
2024-06-27 11:43:59 +0200

    wip add auth change method button

--- src/app/account/auth/auth.controller.ts
+++ src/app/account/auth/auth.controller.ts
@@ -1,13 +1,12 @@
-import { ActionSheetButton, ActionSheetController, ActionSheetOptions, ModalController, PopoverController } from '@ionic/angular';
+import { ActionSheetController, ActionSheetOptions, ModalController, PopoverController } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { PlatformService } from '@app/shared/services/platform.service';
 import { PopoverOptions } from '@ionic/core';
-import { ListPopover, ListPopoverOptions } from '@app/shared/popover/list.popover';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthModal, AuthModalOptions } from '@app/account/auth/auth.modal';
 import { Router } from '@angular/router';
 import { RegisterModal, RegisterModalOptions } from '@app/account/register/register.modal';
-import { Account, LoginMethods, LoginOptions, SelectAccountOptions, UnlockOptions } from '@app/account/account.model';
+import { Account, LoginOptions, SelectAccountOptions, UnlockOptions } from '@app/account/account.model';
 import { AuthV2Modal } from '@app/account/auth/authv2.modal';
 import { UnlockModal } from '@app/account/unlock/unlock.modal';
 import { AccountListComponent, AccountListComponentInputs } from '@app/account/list/account-list.component';
@@ -43,37 +42,8 @@ export class AuthController implements IAuthController {
 
     // Ask login method
     if (!loginMethod) {
-      // ...using popover
-      if (!this._mobile) {
-        const popover = await this.popoverCtrl.create(<PopoverOptions>{
-          event,
-          backdropDismiss: true,
-          component: ListPopover,
-          cssClass: 'login-method-popover',
-          componentProps: <ListPopoverOptions>{
-            title: 'LOGIN.METHOD_POPOVER_TITLE',
-            items: LoginMethods,
-          },
-        });
-        await popover.present(event);
-        const { data } = await popover.onWillDismiss();
-        loginMethod = data;
-      } else {
-        const actionSheet = await this.actionSheetCtrl.create({
-          ...this.actionSheetOptions,
-          header: this.translate.instant('LOGIN.METHOD_POPOVER_TITLE'),
-          buttons: LoginMethods.map((method) => {
-            return <ActionSheetButton>{
-              id: method.value,
-              data: method.value,
-              text: this.translate.instant(method.label),
-            };
-          }),
-        });
-        await actionSheet.present();
-        const { data } = await actionSheet.onWillDismiss();
-        loginMethod = data;
-      }
+      // TODO take prefered method from config
+      loginMethod = 'v1';
     }
     if (!loginMethod) return undefined; // User cancelled
 
